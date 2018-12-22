495aac2d646a8dbe8355295fd50ffc8544a312b2
Логист <@LOGIST>
2018-12-22 15:33:08 +0200

    add MVC

--- WebApplicationTestDB/Global.asax.cs
+++ WebApplicationTestDB/Global.asax.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using WebApplicationTestDB.Controllers;
@@ -15,6 +18,10 @@ namespace WebApplicationTestDB
         protected void Application_Start(object sender, EventArgs e)
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
         protected void Session_Start(object sender, EventArgs e)
